6d7291f42c27b0cf6c0dd2aa245fa74bcb2d176d
Maria Comas <unamaria@gmail.com>
2018-11-25 12:26:03 +0100

    Get organization.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
+import ApolloClient, { gql } from 'apollo-boost';
+import 'cross-fetch/polyfill';
+
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+      description
+    }
+  }
+`;
 
-console.log(user);
 
-console.log(process.env.SOME_ENV_VARIABLE);
+client.
+  query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
